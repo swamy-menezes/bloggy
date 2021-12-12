1fab7a5657fe4ef3ba241b10dffcb86cca7582aa
swamy-menezes <swamy.menezes@gmail.com>
2021-12-11 22:40:12 -0400

    chore: setting up mongoose, dotenv and create batabase connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
 import routes from './routes'
 
 const server = express()
 const port = process.env.PORT || 3000
+mongoose.connect(`mongodb+srv://swamy:${process.env.DB_PASSWORD}@cluster0.hytdh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
 
 server.use(express.json())
 server.use(routes)
